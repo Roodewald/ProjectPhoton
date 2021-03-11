9ab489e7bf68b12e7baae2d6b3f56c5ef5a57e2c
Roodewald SP4 <Pavel999977@gmail.com>
2021-03-11 15:43:01 +0500

    Практически добавил списки

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    public static LobbyManager manager;
+
     [SerializeField] TMP_InputField nicknameField;
     [SerializeField] TMP_InputField roomNameField;
     [SerializeField] MenuManager menuManager;
+    [SerializeField] TMP_Text roomNameText;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+
+    private void Awake()
+    {
+        manager = this;
+    }
+
     void Start()
     {
         //EnableLoadingMenu
@@ -16,7 +29,6 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
         PhotonNetwork.NickName = "Player " + Random.Range(1, 999);
 
-        PhotonNetwork.GameVersion = "0";
         PhotonNetwork.ConnectUsingSettings();
     }
 
@@ -32,21 +44,53 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         {
             return;
         }
+        PhotonNetwork.CreateRoom(roomNameField.text);
     }
-
-    public void JoinRoom()
+    
+    public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        PhotonNetwork.JoinRandomRoom();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        menuManager.Open(menuManager.menus[4]);
+        errorText.text = "Create room failed " + message + " With code :" + returnCode;
     }
 
     public void ReNicknameMe()
     {
         PhotonNetwork.NickName = nicknameField.text;
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        // foreach (Transform trans in roomListContent)
+        //  {
+        //    Destroy(trans.gameObject);
+        //  }
+        Debug.Log("FindRoom!!!!");
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public void JoinLobby(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        menuManager.Open(menuManager.menus[0]);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        menuManager.Open(menuManager.menus[5]);
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public void LeaveRoom()
+    {
+        menuManager.Open(menuManager.menus[1]);
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        menuManager.Open(menuManager.menus[1]);
     }
 }
---
+++ Assets/Scripts/RoomListItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Realtime;
+
+public class RoomListItem : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+   
+    RoomInfo info;
+
+    public void SetUp(RoomInfo roomInfo)
+    {
+        info = roomInfo;
+        text.text = roomInfo.Name;
+    }
+    public void OnClick()
+    {
+        LobbyManager.manager.JoinLobby(info);
+    }
+}
